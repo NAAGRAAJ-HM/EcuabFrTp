56bd4e1ede8e8dc41dd19cf3d17b1f120afed2b5
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:51:46 +0100

    Update: AUTOSAR basic software ECU abstraction module for flexray communication protocol stack - transport layer

---
+++ InfineonTLE9879EvalKit_R2023/infEcuabFrTp_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infEcuabFrTp_ServiceNvM.hpp                                       */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTECUABFRTP_AR_RELEASE_VERSION_MAJOR                                4
+#define CONSTECUABFRTP_AR_RELEASE_VERSION_MINOR                                3
+
+#define Const_DefaultInit_ConstEcuabFrTp                                                                             \
+   ,  {CONSTECUABFRTP_AR_RELEASE_VERSION_MAJOR, CONSTECUABFRTP_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
